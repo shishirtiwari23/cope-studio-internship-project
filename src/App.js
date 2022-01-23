4f9f42c6e87ff722c99d270626882b64d9dad308
Shishir Tiwari <shishir456tiwari@gmail.com>
2022-01-23 19:00:47 +0530

    gh-pages added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import styles from "./App.module.scss";
 import { Home } from "./pages";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (
-    <div className={styles.container}>
-      <Home />
-    </div>
+    <Router basename="/cope-studio-internship-project">
+      <div className={styles.container}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </Router>
   );
 };
 
