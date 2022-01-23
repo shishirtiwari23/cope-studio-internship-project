3cd22b7612f04475b664ebc6a279f7be87720704
Shishir Tiwari <shishir456tiwari@gmail.com>
2022-01-23 12:07:16 +0530

    boilerplate initialized

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import styles from "./App.module.scss";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.container}>
+      <h1>Checking</h1>
+      <h2>Checking</h2>
+      <h4>Checking</h4>
+      <p>Checking</p>
     </div>
   );
-}
+};
 
 export default App;
